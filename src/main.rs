0dcf45fbc7efa09949f935347c896dc78c05b4f0
sailfish009 <sailfish009@gmail.com>
2018-03-22 12:07:54 +0900

    edit

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,31 @@
+//========================================================================
+// read - simple win32 text editor written in rust
+//------------------------------------------------------------------------
+// Copyright (c) 2018 Ji Wong Park <sailfish009@gmail.com>
+//
+// This software is provided 'as-is', without any express or implied
+// warranty. In no event will the authors be held liable for any damages
+// arising from the use of this software.
+//
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it
+// freely, subject to the following restrictions:
+//
+// 1. The origin of this software must not be misrepresented; you must not
+//    claim that you wrote the original software. If you use this software
+//    in a product, an acknowledgment in the product documentation would
+//    be appreciated but is not required.
+//
+// 2. Altered source versions must be plainly marked as such, and must not
+//    be misrepresented as being the original software.
+//
+// 3. This notice may not be removed or altered from any source
+//    distribution.
+//
+//========================================================================
+
 // hide console window
-// #![windows_subsystem = "windows"]
+#![windows_subsystem = "windows"]
 
 const SX:i32 = 200; // window x
 const SY:i32 = 200; // window y
@@ -33,6 +59,8 @@ use std::os::windows::ffi::OsStrExt;
 use std::ffi::OsStr;
 use std::ptr;
 
+static MODE: u8 = 0;
+
 fn to_wstring(str : &str) -> Vec<u16> 
 {
   let v : Vec<u16> =
@@ -40,12 +68,22 @@ fn to_wstring(str : &str) -> Vec<u16>
   v
 }
 
+fn edit(w :WPARAM)
+{
+  match w 
+  {
+    _ => (),
+  }
+}
+
 pub unsafe extern "system" fn window_proc(h_wnd :HWND, 
   msg :UINT, w_param :WPARAM, l_param :LPARAM) -> LRESULT
 {
-  if msg == user::WM_DESTROY 
+  match msg 
   {
-    user::PostQuitMessage(0);
+    user::WM_CHAR => edit(w_param),
+    user::WM_DESTROY => user::PostQuitMessage(0),
+    _ => (),
   }
   return user::DefWindowProcW( h_wnd, msg, w_param, l_param);
 }
@@ -76,7 +114,7 @@ fn main()
       to_wstring("read v0.1").as_ptr(),
       WS_OVERLAPPEDWINDOW | WS_VISIBLE, 0, 0, W, H, 0 as HWND, 0 as HMENU, 0 as HINSTANCE, ptr::null_mut());
   
-    user::InvalidateRect(hwnd, ptr::null(), 1);
+    // user::InvalidateRect(hwnd, ptr::null(), 1);
     user::ShowWindow(hwnd, user::SW_SHOW);
       
     let mut msg = user::MSG 
@@ -89,8 +127,8 @@ fn main()
       pt : winapi::shared::windef::POINT{x:0, y:0, }, 
     };
 
-	// background
-    let brush  =  gdi::CreateSolidBrush(gdi::RGB(0,0,0)) as i32;
+    // background
+    let brush = gdi::CreateSolidBrush(gdi::RGB(0,0,0)) as i32;
     user::SetClassLongPtrW(hwnd, user::GCLP_HBRBACKGROUND, brush);
     user::MoveWindow(hwnd, SX, SY, W, H, 1);
 
