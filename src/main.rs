ba7f7d31774d4b171275f1d51e739fffbf81e7f8
sailfish009 <sailfish009@gmail.com>
2018-06-01 08:34:11 +0900

    delete char

--- src/main.rs
+++ src/main.rs
@@ -29,13 +29,11 @@
 
 #[macro_use]
 extern crate lazy_static;
+extern crate winapi; 
 
 const SX: i32 = 200; const SY: i32 = 200; const W:  i32 = 800; const H:  i32 = 600;
 const R_A: u8 = 250; const G_A: u8 = 250; const B_A: u8 = 250;
 const R_B: u8 = 0;   const G_B: u8 = 0;   const B_B: u8 = 0;
-const R:   u8 = 0;   const L:   u8 = 1; 
-
-extern crate winapi; 
 
 use winapi::um::shellapi as shell;
 use winapi::um::winuser as user;     
@@ -90,7 +88,6 @@ fn to_wstring(str : &str) -> Vec<u16>
 fn fileio(w :HWND, f :HFONT, path :String, mode :u8)
 {
   let mut result = File::open(path);
-
   match result
   {
     Ok(mut result) =>
@@ -110,12 +107,12 @@ fn fileio(w :HWND, f :HFONT, path :String, mode :u8)
     
           for c in buffer.chars()
           {
+            let x = {POS.lock().unwrap().x};
+            let y = {POS.lock().unwrap().y};
             match c
             {
               '\r' =>
               {
-                let x = {POS.lock().unwrap().x};
-                let y = {POS.lock().unwrap().y};
                 let ch = CH{x:x, y:y,c:c};
                 save(ch);
                 POS.lock().unwrap().x = 0;
@@ -124,8 +121,6 @@ fn fileio(w :HWND, f :HFONT, path :String, mode :u8)
               '\n' =>{},
               _ => 
               { 
-                let x = {POS.lock().unwrap().x};
-                let y = {POS.lock().unwrap().y};
                 let ch = CH{x:x,y:y,c:c};
                 drawtext(w, f, ch, 0); 
                 POS.lock().unwrap().x += 1;
@@ -163,65 +158,21 @@ fn clearscreen(w :HWND)
     user::RedrawWindow(w, ptr::null_mut(), ptr::null_mut(), user::RDW_INVALIDATE | user::RDW_UPDATENOW);
   }
 }
-
 fn clearch(w :HWND, i :usize, r :i32)
 {
   let rect = getrect(i);
   unsafe {user::InvalidateRect(w, &rect, r);}
 }
-
-fn clear()
-{
-  let mut vec = {TEXT.lock().unwrap()};
-  vec.clear();
-}
-
-fn remove(i :usize)
-{
-  let mut vec = {TEXT.lock().unwrap()};
-  vec.remove(i);
-}
-
-fn insert(i :usize, c :CH)
-{
-  let mut vec = {TEXT.lock().unwrap()};
-  vec.insert(i, c)
-}
-
-fn save(c :CH)
-{
-  let mut vec = {TEXT.lock().unwrap()};
-  vec.push(c);
-}
-
-fn getlength() -> usize
-{
-  let vec = {TEXT.lock().unwrap()};
-  let length = vec.len();
-  length
-}
-
-fn getx(i :usize) -> LONG 
-{
-  let vec = {TEXT.lock().unwrap()};
-  let x = vec[i].x;
-  x
-}
-
-fn gety(i :usize) -> LONG 
-{
-  let vec = {TEXT.lock().unwrap()};
-  let y = vec[i].y;
-  y 
-}
-
-fn getc(i :usize) -> char 
-{
-  let vec = {TEXT.lock().unwrap()};
-  let c = vec[i].c;
-  c 
-}
-
+fn clear() {let mut vec = {TEXT.lock().unwrap()}; vec.clear();}
+fn remove(i :usize){let mut vec = {TEXT.lock().unwrap()}; vec.remove(i);}
+fn insert(i :usize, c :CH){let mut vec = {TEXT.lock().unwrap()}; vec.insert(i, c)}
+fn save(c :CH){let mut vec = {TEXT.lock().unwrap()}; vec.push(c);}
+fn getlength() -> usize {let vec = {TEXT.lock().unwrap()}; let length = vec.len();length}
+fn getx(i :usize) -> LONG {let vec = {TEXT.lock().unwrap()}; let x = vec[i].x; x}
+fn gety(i :usize) -> LONG {let vec = {TEXT.lock().unwrap()}; let y = vec[i].y; y}
+fn getc(i :usize) -> char {let vec = {TEXT.lock().unwrap()}; let c = vec[i].c; c}
+fn setx(i :usize, x :LONG){let mut vec = {TEXT.lock().unwrap()}; vec[i].x = x;}
+fn sety(i :usize, y :LONG){let mut vec = {TEXT.lock().unwrap()}; vec[i].y = y;}
 fn getlastx() -> Option<usize>
 {
   let y = {POS.lock().unwrap().y};
@@ -229,7 +180,6 @@ fn getlastx() -> Option<usize>
   let index = vec.iter().position(|ref e| ((e.c == '\r') && (e.y == y)));
   index
 }
-
 fn getindex() -> Option<usize>
 {
   let x = {POS.lock().unwrap().x};
@@ -238,88 +188,73 @@ fn getindex() -> Option<usize>
   let index = vec.iter().position(|ref e| ((e.x == x) && (e.y == y)));
   index 
 }
-
-fn setx(i :usize, x :LONG)
-{
-  let mut vec = {TEXT.lock().unwrap()};
-  vec[i].x = x;
-}
-
-fn sety(i :usize, y :LONG)
-{
-  let mut vec = {TEXT.lock().unwrap()};
-  vec[i].y = y;
-}
-
-fn backspace() -> Result<usize,usize>
-{
-  // Ok(x)
-  Err(0)
-}
-
 fn getrect(i :usize) -> RECT
 {
-  let vec = {TEXT.lock().unwrap()};
-  let ch_w = {*CHX.lock().unwrap()};
-  let ch_h = {*CHY.lock().unwrap()};
-  let rect = RECT
-  {
-    left:vec[i].x*ch_w, 
-    top:(vec[i].y*ch_h),
-    right:(vec[i].x*ch_w+ch_w),
-    bottom:(vec[i].y*ch_h+ch_h)
-  };
-  rect
+  let v = {TEXT.lock().unwrap()};
+  let w = {*CHX.lock().unwrap()};
+  let h = {*CHY.lock().unwrap()};
+  let r = RECT{left:v[i].x*w,top:(v[i].y*h),right:(v[i].x*w+w),bottom:(v[i].y*h+h)};
+  r
 }
 
-fn line(w :HWND, mode :u8)
+fn line(w :HWND, f :HFONT, mode :u8)
 {
+  let x = {POS.lock().unwrap().x};
+  let y = {POS.lock().unwrap().y};
   match mode
   {
     // delete
     0 =>
     {
-      let x = {POS.lock().unwrap().x};
-      let y = {POS.lock().unwrap().y};
       if (x == 0 && y == 0) {return;}
+      let mut line_end :usize = 0;
+      let lastx = getlastx();
+      match lastx
+      {
+        None => {},
+        _ => {line_end = lastx.unwrap();},
+      }
+
+      if( x > 0 )
+      {
+        POS.lock().unwrap().x -= 1;
+      }
+      else if( y > 0 )
+      {
+        POS.lock().unwrap().y -= 1;
+        POS.lock().unwrap().x = getx(line_end);
+      }
+
       let index = getindex();
       match index
       {
         None => {},
         _ =>
         {
-          let index = index.unwrap();
           hidecaret(w);
-          if x > 0 
+          let index = index.unwrap();
+          for i in index..(line_end+1)
           {
-            POS.lock().unwrap().x -= 1;
-            clearch(w, index-1, 1);
-          }
-          else if y > 0
+            clearch(w, i, 0);
+            setx(i, getx(i)-1);
+          } 
+          remove(index);
+          let end = line_end-1;
+          for i in index..end
           {
-            POS.lock().unwrap().y -= 1;
-            let lastx = getlastx();
-            match lastx
-            {
-              None => {},
-              _ => 
-              {
-                clearch(w, index-1, 1);
-                let index = lastx.unwrap();
-                POS.lock().unwrap().x = getx(index);
-              },
-            }
+            let ch = CH{x:getx(i),y:gety(i),c:getc(i)};
+            drawtext(w, f, ch, 1);
           }
+          // workaround, repaint the trailing char with space
+          let ch = CH{x:getx(end),y:gety(end),c:' '};
+          drawtext(w, f, ch, 1);
           showcaret(w);
         },
       }
-      // for val in vec.iter() {print!("[{0}]", val.c as u8);} println!{""};
     },
     // enter
     1 =>
     {
-      let x = {POS.lock().unwrap().x};
-      let y = {POS.lock().unwrap().y};
       let ch = CH{x:x,y:y,c:'\r'};
       save(ch);
     },
@@ -415,8 +350,8 @@ fn key_right(w :HWND)
 
 fn showcaret(w :HWND)
 {
-  let option = getindex();
-  match option
+  let index = getindex();
+  match index
   {
     None => 
     {
@@ -447,12 +382,12 @@ fn showcaret(w :HWND)
     },
     _ =>
     {
-      let index = option.unwrap();
-      let x     = getx(index);
-      let y     = gety(index);
-      let c     = getc(index);
-      let ch_w  = {*CHX.lock().unwrap()};
-      let ch_h  = {*CHY.lock().unwrap()};
+      let i = index.unwrap();
+      let x = getx(i);
+      let y = gety(i);
+      let c = getc(i);
+      let ch_w = {*CHX.lock().unwrap()};
+      let ch_h = {*CHY.lock().unwrap()};
       unsafe
       {
         user::SetCaretPos(x*ch_w, y*ch_h);
@@ -471,22 +406,22 @@ fn edit(w :HWND, p :WPARAM, f :HFONT)
 {
   if unsafe{user::GetAsyncKeyState(user::VK_CONTROL)} as u16 & 0x8000 != 0
   {
-    println!("p: 0x{0:2x}", p as u8);
+    // println!("p: 0x{0:02x}", p as u8);
     match p 
     {
       // ctrl + o 
       0x0F => 
       {
-        // let path = String::from("./sample.txt");
-        // fileio(w, f, path, 0); 
         let length = getlength();
         println!("debug, length: {0}", length);
         let vec = {TEXT.lock().unwrap()};
-        for val in vec.iter() {print!("[{0}]", val.c as u8);} println!{""};
+        for val in vec.iter() {print!("[{0:02x},{1}]", val.c as u8, val.x);} println!{""};
       },
       // ctrl + x 
       0x18 => 
       {
+        let path = String::from("./test.txt");
+        fileio(w, f, path, 0); 
       },
       0x13 => println!("0x13"),
       0x02 => println!("0x02"),
@@ -528,15 +463,14 @@ fn edit(w :HWND, p :WPARAM, f :HFONT)
               let index = index.unwrap();
               let length = getlength();
               let y = {POS.lock().unwrap().y};
-              let ch_w = {*CHX.lock().unwrap()} as i32;
-              let ch_h = {*CHY.lock().unwrap()};
-
+              let w = {*CHX.lock().unwrap()} as i32;
+              let h = {*CHY.lock().unwrap()};
               for i in index..length
               {
                 if getc(i) == '\r' 
                 {
                   let x = getx(i);
-                  user::SetCaretPos(x*ch_w, y*ch_h);
+                  user::SetCaretPos(x*w, y*h);
                   POS.lock().unwrap().x = x;
                   break;
                 }
@@ -544,13 +478,12 @@ fn edit(w :HWND, p :WPARAM, f :HFONT)
                 {
                   *END.lock().unwrap() = 1;
                   let x = getx(i)+1;
-                  user::SetCaretPos(x*ch_w, y*ch_h);
+                  user::SetCaretPos(x*w, y*h);
                   POS.lock().unwrap().x = x;
                 }
               }
             },
           }
-
         },
         // a: change mode 
         0x61 => 
@@ -582,12 +515,12 @@ fn edit(w :HWND, p :WPARAM, f :HFONT)
     // backspace
     0x08 => 
     {
-      line(w,0);
+      line(w, f, 0);
     },
     // enter 
     0x0D => 
     {
-      line(w,1);
+      line(w, f, 1);
       hidecaret(w);
       POS.lock().unwrap().x = 0;
       POS.lock().unwrap().y += 1;
@@ -645,10 +578,7 @@ fn edit(w :HWND, p :WPARAM, f :HFONT)
 
             for i in index..line_end
             {
-              let c = getc(i);
-              let x = getx(i);
-              let y = gety(i);
-              let ch = CH{x:x as i32,y:y as i32,c:c};
+              let ch = CH{x:getx(i) as i32,y:gety(i) as i32,c:getc(i)};
               drawtext(w, f, ch, 1);  
             }
             POS.lock().unwrap().x += 1;
@@ -803,28 +733,20 @@ fn main()
     gdi::GetCharWidth32W(dc, 0 as UINT, 0 as UINT, &mut char_w); 
     gdi::GetTextMetricsW(dc, &mut tm);
     user::ReleaseDC(hwnd, dc);
-
     *CHX.lock().unwrap() = char_w;
     *CHY.lock().unwrap() = tm.tmHeight;
-
     user::CreateCaret(hwnd, 0 as HBITMAP, 1, tm.tmHeight);
     showcaret(hwnd);
-
     shell::DragAcceptFiles(hwnd, 1);
-
     // background
     // let brush = gdi::CreateSolidBrush(gdi::RGB(R_B,G_B,B_B)) as isize; // 64bit
     let brush = gdi::CreateSolidBrush(gdi::RGB(R_B,G_B,B_B)) as i32;      // 32bit
     user::SetClassLongPtrW(hwnd, user::GCLP_HBRBACKGROUND, brush);
     user::MoveWindow(hwnd, SX, SY, W, H, 1);
-
     loop
     {
       let m = user::GetMessageW(&mut msg, 0 as HWND, 0, 0);
-      if msg.message == user::WM_QUIT
-      {
-        break;
-      }
+      if msg.message == user::WM_QUIT {break;}
       if m > 0
       {
         user::TranslateMessage(&mut msg);
